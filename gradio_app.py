5b30afef0cd742b84710aff8f7f8af35447050cb
Erez Schnaider <erez@clear.ml>
2023-01-30 16:38:16 +0200

    fix bug

--- gradio_app.py
+++ gradio_app.py
@@ -1,10 +1,26 @@
 import gradio as gr
 from clearml import Task
+import torch
+import requests
+from torchvision import transforms
 
-def greet(name):
-    return "Hallo " + name + "!"
+model = torch.hub.load('pytorch/vision:v0.6.0', 'resnet18', pretrained=True).eval()
+response = requests.get("https://git.io/JJkYN")
+labels = response.text.split("\n")
 
-Task.init(project_name='gradio test',task_name='gradio_erez')
-demo = gr.Interface(fn=greet, inputs="text", outputs="text")
+
+def predict(inp):
+    inp = transforms.ToTensor()(inp).unsqueeze(0)
+    with torch.no_grad():
+        prediction = torch.nn.functional.softmax(model(inp)[0], dim=0)
+        confidences = {labels[i]: float(prediction[i]) for i in range(1000)}
+    return confidences
+
+
+demo = gr.Interface(fn=predict,
+                    inputs=gr.inputs.Image(type="pil"),
+                    outputs=gr.outputs.Label(num_top_classes=3),
+                    examples=[["https://www.shutterstock.com/image-photo/surreal-image-african-elephant-wearing-260nw-1365289022.jpg"]],
+                    )
 
 demo.launch()
