3532ac392e69cff2736c1682ed66610343f3e18f
Erez Schnaider <erez@clear.ml>
2023-01-30 15:11:22 +0200

    add task.init

--- gradio_app.py
+++ gradio_app.py
@@ -1,8 +1,10 @@
 import gradio as gr
+from clearml import Task
 
 def greet(name):
     return "Hallo " + name + "!"
 
+Task.init(project_name='gradio test',take_name='gradio_erez')
 demo = gr.Interface(fn=greet, inputs="text", outputs="text")
 
 demo.launch()
